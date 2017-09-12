fc94289bc4d7119c54b0a0452d5585dad0c3a4a9
Timo Mämecke <timo@maemecke.com>
2017-09-12 19:15:30 +0200

    Fix missing return

--- example/SimpleBottomNavigation.js
+++ example/SimpleBottomNavigation.js
@@ -6,34 +6,36 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 
 export default class SimpleBottomNavigation extends Component {
   render() {
-    <View style={{ flex: 1 }}>
-      <BottomNavigation
-        labelColor="white"
-        rippleColor="white"
-        style={styles.bottomNavigation}
-      >
-        <Tab
-          barBackgroundColor="#37474F"
-          label="Movies & TV"
-          icon={<Icon size={24} color="white" name="ondemand-video" />}
-        />
-        <Tab
-          barBackgroundColor="#00796B"
-          label="Music"
-          icon={<Icon size={24} color="white" name="music-note" />}
-        />
-        <Tab
-          barBackgroundColor="#5D4037"
-          label="Books"
-          icon={<Icon size={24} color="white" name="book" />}
-        />
-        <Tab
-          barBackgroundColor="#3E2723"
-          label="Newsstand"
-          icon={<Icon size={24} color="white" name="newspaper" />}
-        />
-      </BottomNavigation>
-    </View>
+    return (
+      <View style={{ flex: 1 }}>
+        <BottomNavigation
+          labelColor="white"
+          rippleColor="white"
+          style={styles.bottomNavigation}
+        >
+          <Tab
+            barBackgroundColor="#37474F"
+            label="Movies & TV"
+            icon={<Icon size={24} color="white" name="ondemand-video" />}
+          />
+          <Tab
+            barBackgroundColor="#00796B"
+            label="Music"
+            icon={<Icon size={24} color="white" name="music-note" />}
+          />
+          <Tab
+            barBackgroundColor="#5D4037"
+            label="Books"
+            icon={<Icon size={24} color="white" name="book" />}
+          />
+          <Tab
+            barBackgroundColor="#3E2723"
+            label="Newsstand"
+            icon={<Icon size={24} color="white" name="newspaper" />}
+          />
+        </BottomNavigation>
+      </View>
+    )
   }
 }
 
